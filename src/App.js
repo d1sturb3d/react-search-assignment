d2e94f5e40edd0bbf654a846f7c08d2b47f9f6d9
Shardul Khadye <f20180945@hyderabad.bits-pilani.ac.in>
2021-04-26 01:40:32 +0530

    slight design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , { useState } from 'react'
 import JSONDATA from './Countries.json'
+import TextField from '@material-ui/core/TextField';
 import "./App.css"
 
 export default function App() {
@@ -8,7 +9,8 @@ const [SearchTerm, setSearchTerm] = useState('')
 
   return (
     <div className="App">
-      <input type="text" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/>
+      {/* <input type="text" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/> */}
+        <TextField className = "form" id="outlined-basic" label="Outlined" variant="outlined" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/>
       {JSONDATA.filter((val) => {
         if (SearchTerm === ""){
           return val
@@ -19,7 +21,7 @@ const [SearchTerm, setSearchTerm] = useState('')
       }).map((val,key) => {
         return (
             <div className="country" key={key}>
-            <p>{val.country}'s capital is {val.city}</p>
+            	<p>{val.country}'s capital is {val.city}</p>
             </div>
           )
       })}
