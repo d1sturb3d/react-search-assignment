5f9ae0621482ccb6f65f37c468fff72c0d6d79f7
Shardul Khadye <f20180945@hyderabad.bits-pilani.ac.in>
2021-04-26 01:24:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React , { useState } from 'react'
+import JSONDATA from './Countries.json'
+import "./App.css"
+
+export default function App() {
+
+const [SearchTerm, setSearchTerm] = useState('')
+
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/>
+      {JSONDATA.filter((val) => {
+        if (SearchTerm === ""){
+          return val
+        }
+        else if (val.country.toLocaleLowerCase().includes(SearchTerm.toLocaleLowerCase())){
+          return val
+        }
+      }).map((val,key) => {
+        return (
+            <div className="country" key={key}>
+            <p>{val.country}'s capital is {val.city}</p>
+            </div>
+          )
+      })}
+    </div>
+  )
+}
