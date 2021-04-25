f505dfc18a726bf31bb14cf08494e3284e37cfca
Shardul Khadye <f20180945@hyderabad.bits-pilani.ac.in>
2021-04-26 02:58:08 +0530

    tried to implement onClick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React , { useState } from 'react'
 import JSONDATA from './Countries.json'
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import "./App.css"
 
 export default function App() {
 
 const [SearchTerm, setSearchTerm] = useState('')
+const [IsClicked, setIsClicked] = useState(0)
+// const handler = (c) =>{
+//    setIsClicked(1);
+// }
 
   return (
     <div className="App">
-      {/* <input type="text" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/> */}
         <TextField className = "form" id="outlined-basic" label="Country Name" variant="outlined" placeholder="Search Country" onChange={event => {setSearchTerm(event.target.value)}}/>
       {JSONDATA.filter((val) => {
         if (SearchTerm === ""){
@@ -20,11 +24,15 @@ const [SearchTerm, setSearchTerm] = useState('')
         }
       }).map((val,key) => {
         return (
-			<div className="wrapper">
+          <Button variant="primary">
+            <div className="wrapper">
             <div className="country" key={key}>
-            	<p>{val.country}'s capital is {val.city}</p>
+            	<p>{val.country}</p>
+              <p>{val.city}</p>
             </div>
-			</div>
+			      </div>
+          </Button>
+			
           )
       })}
     </div>
